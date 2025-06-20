9c5e601beba0e0acb9f9028cdc0896cdd64f4470
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-20 20:11:27 +0200

    WIP: creating and  updating product card

--- src/componets/ProductList.jsx
+++ src/componets/ProductList.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import ProductCard from './ProductCard';
 import '../styles/ProductList.css';
 
-const ProductList = ({ products })=>{
-    if(!products.lenghth) return <p> Loading Products</p>
 
-    return(
-        <div style={{display: 'flex', flexWrap: 'wrap'}}>
-            {products.map(product=>(
-                <div key= {product.id} className="product-card">
-                <img src={product.image} alt ={product.title} />
-                <h4>{product.title}</h4>
-                <p>R{product.price}</p>
-            </div>))}
+const ProductList = ({ products })=>{
+  
+    if (!products.length) return <p>Loading Products...</p>;
+
+
+    // return (
+    //     <div style={{display: 'flex', flexWrap: 'wrap'}}>
+    //         {products.map(product=>(
+    //             <div key= {product.id} className="product-card">
+    //             <img src={product.image} alt ={product.title} />
+    //             <h4>{product.title}</h4>
+    //             <p>R{product.price}</p>
+    //         </div>))}
+    //     </div>
+    //   );
+
+
+    return (
+        <div className="product-list">
+          {products.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
-    );
+      );
+        
+
 };
 export default ProductList
