1b3c352a50199cfea179f8028d2d642e14b9b31b
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-22 15:33:29 +0200

    WIP: adding routing functinality and creating product details page and style

--- src/componets/ProductCard.jsx
+++ src/componets/ProductCard.jsx
@@ -1,15 +1,31 @@
 import { useContext } from "react";
 import {CartContext} from '../context/CartContext';
 import '../styles/ProductList.css';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCard = ({product}) => {
     const { addToCart} = useContext(CartContext);
+    const navigate = useNavigate();
 
-    return(<div className="product-card">
-        <img src={product.image} alt ={product.title} />
-        <h4>{product.title}</h4>
-        <p>R{product.price}</p>
-        <button className="add-btn" onClick={() => addToCart(product)}>Add to Cart</button>
-    </div>);
+    const handleDetails = () => {
+        navigate(`/product/${product.id}`);
+      };
+    return (
+        <div className="product-card">
+          <img
+            src={product.image}
+            alt={product.title}
+            onClick={handleDetails}
+            style={{ cursor: 'pointer' }}
+          />
+          <h4 onClick={handleDetails} style={{ cursor: 'pointer' }}>
+            {product.title}
+          </h4>
+          <p>R{product.price}</p>
+          <button className="add-btn" onClick={() => addToCart(product)}>
+            Add to Cart
+          </button>
+        </div>
+      );
 };
 export default ProductCard;
--- src/services/productService.js
+++ src/services/productService.js
@@ -13,3 +13,18 @@ export const fetchAllProducts = async() => {
         throw error;
     }
 };
+
+export const fetchProduct = async(id) => {
+    try{
+        const response = await fetch(`${BASE_URL}/products/${id}`);
+
+        if(!response.ok) throw new Error(`HTTP error Status: ${response.status}`);
+
+        const data = await response.json();
+         return data;
+ 
+    }catch(error){
+        console.error('Failed to fetch all products:', error);
+        throw error;
+    }
+};
