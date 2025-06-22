2b16261e44e9ba5a8d17b4974160a3a87f717e5d
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-22 15:34:22 +0200

    Chore: adding signup to login popup and updating styling

--- src/componets/LoginPopup.jsx
+++ src/componets/LoginPopup.jsx
@@ -4,13 +4,19 @@ import '../styles/LoginPopUp.css'
 
 const LoginPopup = ({ close }) => {
     const { login } = useContext(AuthContext);
+    const [isLoginView, setLoginView] = useState(true);
     const [form, setForm] = useState({ username: '', password: '' });
+    const [error, setError] = useState('');
   
-    const handleChange = (e) =>
+    const handleChange = (e) => {
       setForm({ ...form, [e.target.name]: e.target.value });
+      setError('');
+    }
   
     const handleSubmit = async (e) => {
       e.preventDefault();
+      setError('');
+
       try {
         await login(form.username, form.password);
         close();
@@ -21,12 +27,21 @@ const LoginPopup = ({ close }) => {
   
     return (
       <div className="login-popup">
-        <h1>Login</h1>
+         <button className="close-btn" onClick={close}>X</button>
+        <h1>{isLoginView ? 'Login' :'Sign Up'}</h1>
         <form onSubmit={handleSubmit}>
-          <input name="username" placeholder="Username" onChange={handleChange} />
-          <input name="password" type="password" placeholder="Password" onChange={handleChange} />
-          <button type="submit">Login</button>
+
+        { !isLoginView &&(  <input name="username" placeholder="Username" onChange={handleChange} value={form.username}/>)}
+          <input name="email" placeholder="Email" onChange={handleChange} value={form.email}/>
+          <input name="password"  value={form.password} type="password" placeholder="Password" onChange={handleChange} />
+          <button className="auth-btn" type="submit">{isLoginView ? 'Login' :'Sign Up'}</button>
         </form>
+        <div className="auth-toggle">
+          {
+            isLoginView? (<p>Don't have an account? <span>Sign Up</span></p>):
+            ( <p>Already have an account? <span>Login</span></p>)
+          }
+        </div>
       </div>
     );
   };
