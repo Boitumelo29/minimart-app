80e42f672925095f6b87462bb3c5f49391938ca5
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-22 15:32:11 +0200

    Chore: adding remove from cart functionality

--- src/componets/CartPopup.jsx
+++ src/componets/CartPopup.jsx
@@ -4,7 +4,7 @@ import '../styles/CartPopup.css'
 
 
 const CartPopup =({close}) => {
-    const {cartItems} = useContext(CartContext);
+    const {cartItems, removeFromCart} = useContext(CartContext);
     const totalItems = cartItems.reduce((a,b)=> a + b.quantity, 0);
 
     return(
@@ -12,15 +12,19 @@ const CartPopup =({close}) => {
             <button className="close-btn" onClick={close}>X</button>
             <h3>🛒 Cart ({totalItems})</h3>
             <ul>
-                { cartItems.map((product, idx)=> (
-                        <li key={idx}>
-                            {<img src={product.image} alt={product.title}/>}
-                            {product.title} — Qty: {product.quantity}
-                        </li>
-                    ))
-                    }
-                    {cartItems.length === 0 && <p> No items in cart</p>}
-            </ul>
+    {cartItems.map((product, idx) => (
+      <li key={idx} className="cart-item">
+         <img src={product.image} alt={product.title} />
+         <div className="cart-item-details">
+         <strong className="cart-item-title">{product.title}</strong>
+         <span className="cart-item-qty">Qty: {product.quantity}</span>
+         </div>
+         <button className="remove-btn" onClick={() => removeFromCart(product.id)}>
+              🗑️
+            </button>
+        </li>
+     ))}
+</ul>
             {cartItems && cartItems.length > 0 && (<button className="check-out"><span>Check Out</span></button>
 )}
         </div>
