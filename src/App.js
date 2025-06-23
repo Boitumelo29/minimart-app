ac24123bd1f794fbdbce97d3f1a9a002d704ffe5
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-23 14:05:16 +0200

    Chore: removing navbar from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {CartProvider} from './context/CartContext';
-import Navbar from './componets/Navbar';
 import { AuthProvider } from './context/AuthContext';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
@@ -9,7 +8,6 @@ function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <Navbar />
         <RouterProvider router={router} />
       </CartProvider>
     </AuthProvider>
