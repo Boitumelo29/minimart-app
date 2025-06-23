a830a70249ea751e59a2b80b77135dbe71175058
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-23 14:07:23 +0200

    WIP: updating router format and adding layout as main directory and creating layout.jsx

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import ProductDetails from '../pages/ProductDetails';
+import Checkout from '../pages/Checkout';
+import Layout from '../layout/Layout';
+
 
 export const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-        path: 'product/:id',
-        element: <ProductDetails />
-    }
-  ])
+  {
+    path: '/',
+    element: <Layout />,  
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'product/:id', element: <ProductDetails /> },
+      { path: 'checkout', element: <Checkout /> }
+    ]
+  }
+]);
