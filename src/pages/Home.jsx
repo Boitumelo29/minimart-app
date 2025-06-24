41ec3090fef9057765c98e9dbca4ee632cebded5
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-24 18:30:53 +0200

    WIP: removin unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ProductList from "../componets/ProductList";
-import { fetchAllProducts } from "../services/productService";
 import HeroSection from "../componets/HeroSection";
 import ProductSection from "../componets/ProductSection";
 
