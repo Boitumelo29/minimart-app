c0c823e51ec07c16fc2bed39abff37afe137cc83
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-24 18:25:22 +0200

    WIP: adding navigation to all products in the heroSection

--- src/componets/HeroSection.jsx
+++ src/componets/HeroSection.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import '../styles/HeroSection.css';
 import minimart from '../assets/minimart.png';
-
+import { useNavigate } from "react-router-dom";
 
 
 const HeroSection = () => {
+
+    const navigate = useNavigate();
+
+    const handleProduct = () => {
+        navigate(`/allProducts`);
+    };
     return (
         <div>
             <section className="hero">
@@ -12,7 +18,7 @@ const HeroSection = () => {
                     <h1>Mini Mart</h1>
                     <h3>Shopping made<span className="typing" id="typing"> Easy</span></h3>
                     <p>Shop all your favouite products.</p>
-                    <button className="btn"><span>View All Products</span></button>
+                    <button onClick={handleProduct} className="btn"><span>View All Products</span></button>
                 </div>
                 <img src={minimart} alt="logo" />
             </section>
