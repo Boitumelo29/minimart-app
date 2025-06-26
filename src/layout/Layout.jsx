5f88de78b8a1dae023d92a0ba8758aa53eebc636
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-26 09:45:52 +0200

    WIP: adding footer to layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../componets/Navbar';
+import Footer from '../componets/Footer';
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer/>
     </>
   );
 };
